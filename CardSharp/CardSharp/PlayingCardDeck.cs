f0a57f1e88ca9f6209c211302ef00aed36cfc62c
Skorps123 <fprosado0@gmail.com>
2020-10-28 16:12:30 -0500

    Nullable enabled + additions to PlayingCardDeck

--- CardSharp/CardSharp/PlayingCardDeck.cs
+++ CardSharp/CardSharp/PlayingCardDeck.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Cards.Core;
 
 namespace Cards
 {
-    public class PlayingCardDeck : PlayingCardDeck<StandardSuits> { }
+    public class PlayingCardDeck : PlayingCardDeck<StandardSuits>
+    {
+        public PlayingCardDeck(int setsInDeck, int jokerPerSet, Random? random = null) : base(setsInDeck, jokerPerSet, random)
+        {
+        }
+    }
 
     public class PlayingCardDeck<TSuit> : IGroupedDeck<PlayingCard<TSuit>, TSuit, PlayingCardValues>
          where TSuit : struct, Enum
     {
-        public int CardAmount => throw new NotImplementedException();
+        private readonly Random rng;
+        private readonly Stack<PlayingCard<TSuit>> dealPile;
+        private readonly List<PlayingCard<TSuit>> discardPile;
 
-        public PlayingCardDeck()
+ 
+        /// <inheritdoc/>
+        public int CardAmount { get; }
+ 
+        public PlayingCardDeck(int setsInDeck, int jokerPerSet, Random? random = null)
         {
+            rng = random ?? new Random();
+            const int suitedCardsInSet = 52;
+            CardAmount = (suitedCardsInSet + jokerPerSet) * setsInDeck;
 
+
+            dealPile = new Stack<PlayingCard<TSuit>>(CardAmount);
+            discardPile = new List<PlayingCard<TSuit>>(CardAmount);
+            //TO DO:
+            //-ADD CARDS TO DISCARD PILE
+            //-SHUFFLE CARDS IN DISCARD AND REDEAL THEM
+            //-ADD CARDS DEAL PILE
         }
 
         public void AddToDiscard(PlayingCard<TSuit> card)
